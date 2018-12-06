44a66d4774299b29f930f5031322c23114df595c
DESKTOP-NVGPU6S\user <thankosein@gmail.com>
2018-12-06 21:19:35 +0800

    no message

---
+++ PeaceOfHeart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PeaceOfHeart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
